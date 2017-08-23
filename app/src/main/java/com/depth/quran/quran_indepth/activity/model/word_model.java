c8255aabd0028bdd5a92f29bcef2c3fdd4f59ea5
belal <belalmd016@gmail.com>
2017-08-24 02:42:41 +0600

    commit by bellal

---
+++ app/src/main/java/com/depth/quran/quran_indepth/activity/model/word_model.java
@@ -0,0 +1,126 @@
+package com.depth.quran.quran_indepth.activity.model;
+
+/**
+ * Created by DAFFODIL on 8/21/2017.
+ */
+
+public class word_model {
+
+    private String WordId;
+    private String VerseId;
+    private String WordNum;
+    private String WordAr;
+    private String WordMeaningEng;
+    private String MeaningUr;
+    private String RootWordAr;
+    private String RootWordCode;
+    private String RootWordId;
+    private String LetterCount;
+    private String SerialSum;
+    private String WeightSum;
+    private String WordChapterId;
+
+    public String getWordId() {
+        return WordId;
+    }
+
+    public void setWordId(String wordId) {
+        WordId = wordId;
+    }
+
+    public String getVerseId() {
+        return VerseId;
+    }
+
+    public void setVerseId(String verseId) {
+        VerseId = verseId;
+    }
+
+    public String getWordNum() {
+        return WordNum;
+    }
+
+    public void setWordNum(String wordNum) {
+        WordNum = wordNum;
+    }
+
+    public String getWordAr() {
+        return WordAr;
+    }
+
+    public void setWordAr(String wordAr) {
+        WordAr = wordAr;
+    }
+
+    public String getMeaningEng() {
+        return WordMeaningEng;
+    }
+
+    public void setMeaningEng(String meaningEng) {
+        WordMeaningEng = meaningEng;
+    }
+
+    public String getMeaningUr() {
+        return MeaningUr;
+    }
+
+    public void setMeaningUr(String meaningUr) {
+        MeaningUr = meaningUr;
+    }
+
+    public String getRootWordAr() {
+        return RootWordAr;
+    }
+
+    public void setRootWordAr(String rootWordAr) {
+        RootWordAr = rootWordAr;
+    }
+
+    public String getRootWordCode() {
+        return RootWordCode;
+    }
+
+    public void setRootWordCode(String rootWordCode) {
+        RootWordCode = rootWordCode;
+    }
+
+    public String getRootWordId() {
+        return RootWordId;
+    }
+
+    public void setRootWordId(String rootWordId) {
+        RootWordId = rootWordId;
+    }
+
+    public String getLetterCount() {
+        return LetterCount;
+    }
+
+    public void setLetterCount(String letterCount) {
+        LetterCount = letterCount;
+    }
+
+    public String getSerialSum() {
+        return SerialSum;
+    }
+
+    public void setSerialSum(String serialSum) {
+        SerialSum = serialSum;
+    }
+
+    public String getWeightSum() {
+        return WeightSum;
+    }
+
+    public void setWeightSum(String weightSum) {
+        WeightSum = weightSum;
+    }
+
+    public String getWordChapterId() {
+        return WordChapterId;
+    }
+
+    public void setWordChapterId(String wordChapterId) {
+        WordChapterId = wordChapterId;
+    }
+}
